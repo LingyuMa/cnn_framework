447ad4a8b26115198f8086738a994f20ae44652f
Lingyu Ma <malingyu1991@gmail.com>
2018-01-07 21:09:27 -0800

    feat: add functions to read images

---
+++ typedef.py
@@ -0,0 +1,16 @@
+class ProjectType:
+    classification = 0
+    detection = 1
+    segmentation = 2
+
+
+class ImageType:
+    rgb = 0
+    gray = 1
+    binary = 2
+    rgba = 3
+
+
+class LabelType:
+    text = 0
+    image = 1
