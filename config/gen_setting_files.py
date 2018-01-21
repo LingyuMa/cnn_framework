6bac66e0058f7f4be4b8e6abe0d9f12b8bbce73a
Lingyu Ma <malingyu1991@gmail.com>
2018-01-20 21:44:37 -0800

    chore: make gen_setting_files functional

--- config/gen_setting_files.py
+++ config/gen_setting_files.py
@@ -1,17 +1,20 @@
+import argparse
 import json
 import random
 import os
 from os.path import join, abspath
+import sys
+sys.path.append(os.path.abspath(os.curdir))
 
 from typedef import *
 
 
-def config(output_folder):
+def config(output_folder, random_flag=False, number=1):
     output_folder = abspath(output_folder)
     if not os.path.isdir(output_folder):
         os.makedirs(output_folder)
 
-    for i in range(5):
+    for i in range(number):
         configuration = {}
 
         # pre-processing of input image (offline)
@@ -46,12 +49,21 @@ def config(output_folder):
         configuration['threads'] = 8
 
         configuration['optimizer'] = Optimizer.Adam
-        configuration['initial_learning_rate'] = random.uniform(1e-5, 1e-4)
+        if random_flag:
+            configuration['initial_learning_rate'] = random.uniform(1e-5, 1e-4)
+        else:
+            configuration['initial_learning_rate'] = 1e-4
         configuration['leaning_rate_decay'] = 0.5
         configuration['decay_iterations'] = 10
         configuration['Optimizer'] = Optimizer.Adam
-        configuration['l2_regularization'] = random.uniform(1e-4, 1e-3)
-        configuration['batch_size'] = random.randrange(32, 128, 32)
+        if random_flag:
+            configuration['l2_regularization'] = random.uniform(1e-4, 1e-3)
+        else:
+            configuration['l2_regularization'] = 0.
+        if random_flag:
+            configuration['batch_size'] = random.randrange(32, 128, 32)
+        else:
+            configuration['batch_size'] = 32
         configuration['n_epochs'] = 20
 
         configuration['train_log_frequency'] = 10
@@ -69,4 +81,12 @@ def config(output_folder):
 
 
 if __name__ == "__main__":
-    config('/home/lingyu/setting_folders')
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-f", "--file", help="config files location")
+    parser.add_argument("-r", "--random", help="whether to generate randomly", action='store_true')
+    parser.add_argument("-n", "--number", type=int, default=1, help="config files number")
+    args = parser.parse_args()
+    if args.random:
+        config(args.file, True, args.number)
+    else:
+        config(args.file, False, args.number)
