3cca27a26708a609ed74061b60677f539b6c1731
Lingyu Ma <malingyu1991@gmail.com>
2018-02-10 00:42:10 -0800

    feat: add the crop function

--- data/dataset/crop_tiles.py
+++ data/dataset/crop_tiles.py
@@ -7,13 +7,51 @@ sys.path.append(abspath(os.curdir))
 import numpy as np
 from skimage.io import imread, imsave
 
+
+def crop(image, tile_size, ratio, in_padding):
+    rval = []
+    w = image.shape[1]
+    h = image.shape[0]
+    inc_w = int(tile_size * ratio)
+    inc_h = int(tile_size * ratio)
+    w_range = np.arange(0, w, inc_w)
+    h_range = np.arange(0, h, inc_h)
+    for x in w_range:
+        for y in h_range:
+            rval.append(get_region(image, x - in_padding, x + tile_size + in_padding,
+                                   y - in_padding, y + tile_size + in_padding))
+    return rval
+
+
+def get_region(img, x_min, x_max, y_min, y_max):
+    if x_min >= 0 and x_max < img.shape[1] and y_min >= 0 and y_max < img.shape[0]:
+        return img[y_min:y_max, x_min:x_max]
+    new_w = x_max - x_min
+    new_h = y_max - y_min
+    if img.ndim == 2:
+        crop_patch = np.zeros((new_h, new_w)).astype(np.uint8)
+    elif img.ndim == 3:
+        crop_patch = np.zeros((new_h, new_w, img.shape[2])).astype(np.uint8)
+    else:
+        raise NotImplementedError('unknown image type')
+    new_x_min = max(x_min, 0)
+    new_x_max = min(x_max, img.shape[1])
+    new_y_min = max(y_min, 0)
+    new_y_max = min(y_max, img.shape[0])
+    offset_x = new_x_min - x_min
+    offset_y = new_y_min - y_min
+    crop_patch[offset_y: offset_y + new_y_max - new_y_min, offset_x: offset_x + new_x_max - new_x_min] = \
+        img[new_y_min: new_y_max, new_x_min: new_x_max]
+    return crop_patch
+
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("-i", "--images", help="input images path")
     parser.add_argument("-l", "--labels", help="label images path")
     parser.add_argument("-n", "--number", type=int, default=32, help="tile size")
-    parser.add_argument("-r", "--ratio", type=float, default=0.5, help="overlapping ratio")
-    parser.add_argument("-p", "--padding", action='store_true', help="whether create padding tile")
+    parser.add_argument("-r", "--ratio", type=float, default=0.5, help="not overlapping ratio")
+    parser.add_argument("-p", "--padding", type=int, default=0, help="padding pixels")
     parser.add_argument("-o", "--output", help="output folder")
     args = parser.parse_args()
 
@@ -32,38 +70,19 @@ if __name__ == "__main__":
 
     tile_size = args.number
     ratio = args.ratio
+    in_padding = args.padding
     for idx, img_name in enumerate(images_list):
         image = imread(join(images_base_dir, img_name))
         label = imread(join(labels_base_dir, 'label_' + img_name))
-        w = image.shape[1]
-        h = image.shape[0]
-        inc_w = int(tile_size * ratio)
-        inc_h = int(tile_size * ratio)
-        num_w = int(np.ceil(w / inc_w))
-        num_h = int(np.ceil(h / inc_h))
-
         i = 0
-        for x in range(num_w):
-            for y in range(num_h):
-                if y * inc_h + tile_size < h and x * inc_w + tile_size < w:
-                    crop_image = image[y * inc_h: y * inc_h + tile_size, x * inc_w: x * inc_w + tile_size]
-                    crop_label = label[y * inc_h: y * inc_h + tile_size, x * inc_w: x * inc_w + tile_size]
-                else:
-                    crop_image = np.zeros([tile_size, tile_size, image.shape[2]]).astype(np.uint8)
-                    crop_label = np.zeros([tile_size, tile_size]).astype(np.uint8)
-                    y_crop = h - y * inc_h if y * inc_h + tile_size >= h else tile_size
-                    x_crop = w - x * inc_w if x * inc_w + tile_size >= w else tile_size
-                    crop_image[:y_crop, :x_crop] = image[y * inc_h: y * inc_h + y_crop,
-                                                         x * inc_w: x * inc_w + x_crop]
-                    crop_label[:y_crop, :x_crop] = label[y * inc_h: y * inc_h + y_crop,
-                                                         x * inc_w: x * inc_w + x_crop]
-                i += 1
+        for crop_image_tile, crop_label_tile in \
+                zip(crop(image, tile_size, ratio, in_padding), crop(label, tile_size, ratio, 0)):
+            img_out_name = str(idx) + '_' + str(i) + '.png'
+            label_out_name = str(idx) + '_' + str(i) + '.png'
 
-                img_out_name = str(idx) + '_' + str(i) + '.png'
-                label_out_name = str(idx) + '_' + str(i) + '.png'
+            imsave(join(images_out_dir, img_out_name), crop_image_tile)
+            imsave(join(labels_out_dir, label_out_name), crop_label_tile)
 
-                imsave(join(images_out_dir, img_out_name), crop_image)
-                imsave(join(labels_out_dir, label_out_name), crop_label)
-
-                file.write(img_out_name + ',' + join(labels_out_dir, label_out_name) + '\n')
+            file.write(img_out_name + ',' + join(labels_out_dir, label_out_name) + '\n')
+            i += 1
     file.close()
